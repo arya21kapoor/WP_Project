a29e72a735d9a4c7d8d53f4d71545fd407652f18
unknown <arya21kapoor@gmail.com>
2021-08-27 23:19:32 +0530

    Added document reading methodology

--- src/Backend_api/Sectors.py
+++ src/Backend_api/Sectors.py
@@ -17,21 +17,41 @@ class Sector_company(Resource):
         parser=reqparse.RequestParser()
         #Adding parser arguments
         parser.add_argument('sector_name',required=False)
-        #parser.add_argument('company_name',required=True)
+        parser.add_argument('company_name',required=False)
+        parser.add_argument('document_type',required=False)
         #Creating argument dictionary
         args=parser.parse_args()
         #sector_list=["Automobile","Constructon","Finance","FMCG","HealthCare","Metals_Chemicals","Power","Technology"]
         sector_file_list=list(os.listdir('D:\WP_project\src\csv_files'))
         print(sector_file_list)
         sector=args['sector_name']
+        company=args['company_name']
+        document=args['document_type']
         #Created list of csv_files under each sector
         csv_list=[]
         if(sector=="Automobile"):
             csv_list=list(os.listdir('D:\WP_project\src\csv_files\Automobile'))
             print("List of excel files:",csv_list)
+            comp=company +".xlsx"
+            if comp in csv_list:
+                string_path= 'D:\WP_project\src\csv_files\Automobile\\'+comp
+                print("File path:",string_path)
+                profit_loss_df=pd.read_excel (string_path,sheet_name="Profit & Loss")
+                print("Profit & Loss df:")
+                print(profit_loss_df)
+                balance_sheet_df=pd.read_excel(string_path,sheet_name="Balance Sheet")
+                print("Balance Sheet df:")
+                print(balance_sheet_df)
+                cashflow_df=pd.read_excel(string_path,sheet_name="Cash Flow")
+                print("Cash Flow df:")
+                print(cashflow_df)
+                return {"data":True} 
+            else:
+                return {"data":False}
         elif(sector=="Construction"):
             csv_list=list(os.listdir('D:\WP_project\src\csv_files\Construction'))
             print("List of excel files:",csv_list)
+
         elif(sector=="Finance"):
             csv_list=list(os.listdir('D:\WP_project\src\csv_files\Finance'))
             print("List of csv files:",csv_list)
@@ -54,7 +74,7 @@ class Sector_company(Resource):
             csv_list=[]
 
         #Returning the csv_files_dictionary
-        return {'data':csv_list}
+        #return {'data':csv_list}
 #Added this class for the endpoint /sectors
 api.add_resource(Sector_company,'/sectors')
 
